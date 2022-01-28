8fa76a44eb4479d82e63f1095012d6e5c52f482a
elsapet <elizabeth@bearer.sh>
2022-01-28 16:26:01 +0200

    feat: add more example

--- Gemfile
+++ Gemfile
@@ -3,6 +3,9 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.7.1'
 
+# using AWS SDK S3
+gem "aws-sdk-s3"
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails', branch: 'main'
 gem 'rails', '~> 6.1.4', '>= 6.1.4.1'
 # Use sqlite3 as the database for Active Record
--- app/javascript/packs/detectedfile.js
+++ app/javascript/packs/detectedfile.js
@@ -5,43 +5,17 @@ require("controllers");
 
 $(() => {
   // PROVIDER LOGO
-  const logoInput = $("#big_bear_api_url");
-  if (logoInput.val()) {
-    const applyImage = () => {
-      const domain = logoInput.val().split("//").reverse()[0].split("/")[0];
-      $(".provider_logo").attr("src", `https://logo.clearbit.com/${domain}`);
-    };
+  $(".provider_logo").attr("src", `https://s3.mish-abc.example.com`);
 
-    $(".slack").attr("src", `https://api.slack.com/${domain}`);
-    $(".slack").attr("src", `https://hooks.slack.com/${domain}`);
+  $(".slack").attr("src", `https://api.slack.com/${domain}`);
+  $(".slack").attr("src", `https://hooks.slack.com/${domain}`);
 
-    $(".new_unknown").attr("src", `https://api.example.com/qwerty`);
+  $(".stripe").attr("src", `https://api.stripe.com/${domain}`);
+  $(".stripe-webhooks-example").attr("src", `https://hooks.stripe.com`);
 
-    $(".stripe").attr("src", `https://api.stripe.com/${domain}`);
-    $(".stripe-webhooks-example").attr("src", `https://hooks.stripe.com`);
-
-    const content = `<li class="file input optional" id="clearbit">
-      <label for="clearbit_image" class="label">Clearbit Image</label>
-      <img id="clearbit_image" class='provider_logo' src='' />
-    </li>`;
-
-    logoInput.parent().after(content);
-
-    if (logoInput.val() != "") {
-      applyImage();
-    }
-    logoInput.change(applyImage);
-  } else {
-    const applyImage = () => {
-      const domain = logoInput.val().split("//").reverse()[0].split("/")[0];
-      $(".provider_logo").attr(
-        "src",
-        `https://api.prod.company.internal/${domain}`,
-        `https://api.staging.company.internal/${domain}`
-      );
-    };
-  }
-
-  $(".test2").attr("src", `https://api.stripe.com/${domain}`);
-  $(".stripe-test3").attr("src", `https://hooks.stripe.com`);
+  $(".provider_logo").attr(
+    "src",
+    `https://api.prod.company.internal/${domain}`,
+    `https://api.staging.company.internal/${domain}`
+  );
 });
