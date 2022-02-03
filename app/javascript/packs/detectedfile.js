1831f3cf2b89ce1cace1effcacdafc620a59d431
elsapet <elizabeth@bearer.sh>
2022-02-03 08:42:36 +0200

    feat: comment out a few examples

--- app/javascript/packs/detectedfile.js
+++ app/javascript/packs/detectedfile.js
@@ -5,17 +5,17 @@ require("controllers");
 
 $(() => {
   // PROVIDER LOGO
-  $(".provider_logo").attr("src", `https://mish.s3.123.amazonaws.com`);
+  // $(".provider_logo").attr("src", `https://mish.s3.123.amazonaws.com`);
 
   $(".slack").attr("src", `https://api.slack.com/${domain}`);
   $(".slack").attr("src", `https://hooks.slack.com/${domain}`);
 
-  $(".stripe").attr("src", `https://api.stripe.com/${domain}`);
-  $(".stripe-webhooks-example").attr("src", `https://hooks.stripe.com`);
+  // $(".stripe").attr("src", `https://api.stripe.com/${domain}`);
+  // $(".stripe-webhooks-example").attr("src", `https://hooks.stripe.com`);
 
-  $(".provider_logo").attr(
-    "src",
-    `https://api.prod.company.internal/${domain}`,
-    `https://api.staging.company.internal/${domain}`
-  );
+  // $(".provider_logo").attr(
+  //   "src",
+  //   `https://api.prod.company.internal/${domain}`,
+  //   `https://api.staging.company.internal/${domain}`
+  // );
 });
