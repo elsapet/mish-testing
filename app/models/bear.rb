d06beed779c0787856227e07de682369cd3c53d6
elsapet <elizabeth@bearer.sh>
2023-11-14 12:06:08 +0200

    feat: add privacy leak

--- app/models/bear.rb
+++ app/models/bear.rb
@@ -1,6 +1,10 @@
 class Bear
   PASSWORD = "Enter password here"
 
+  def privacy_leak
+    raise "Bear not found for #{user.email}"
+  end
+
   def bad
     raise "Bear not found for #{user.email}"
   end
