58c063baf488162749b55ae127fef6e327716309
elsapet <elizabeth@bearer.sh>
2023-12-04 14:50:21 +0200

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   PASSWORD = "Enter password"
   MY_PASSWORD = "Another password"
   API_KEY = "Keep it a secret!"
+  MY_API_KEY = "asdf-qwerty"
 
   def self.log(user)
     Rails.logger.info("it's a user!")
