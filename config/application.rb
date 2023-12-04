575dfd815bdb6a09646ab29bd33c8909e7776dab
elsapet <elizabeth@bearer.sh>
2023-12-04 15:10:37 +0200

    bad code

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module Mish
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.slack_client_id = "qwerty"
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
