afbbc634356cb9a48ff29b7c9c0fe559eda591a3
elsapet <elizabeth@bearer.sh>
2023-12-22 11:15:35 +0200

    feat: bad

---
+++ app/controllers/bears_controller.rb
@@ -0,0 +1,11 @@
+class BearsController < ActionController::Base
+  API_KEY = "Api Key"
+
+  def index
+    Regexp.new(params[:oops])
+  end
+
+  def state
+    change_state unless request.get?
+  end
+end
